149f1166d0753ded6f7f9656ed6c45b22da9e3b6
Akshay.D-eron <akshay.dwivedi@eronmicro.com>
2020-03-19 23:36:07 +0530

    Hooks method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import HooksObject from'./HooksObject' //useState
+// import EffectOne from './EffectOne'  //useEffect
+
+// import FetchRed from './FetchRed'  //data fetch using useReducer and useEffect
+import DataFetch from "./DataFetch"; // data fetch using useEffect
+// import RedUse from './RedUse' // useReducer stateMangement
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <FetchRed/> */}
+      <DataFetch />
+      {/* <HooksObject/> */}
+      {/* <EffectOne/> */}
+      {/* <RedUse/> */}
     </div>
   );
 }
